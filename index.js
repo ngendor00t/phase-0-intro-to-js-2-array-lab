772b00f5412e8316d20b8d76d8bb1a5ede2c4478
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 09:02:28 +0300

    completed assinment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield");
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
+}
+
+function appendCat(name){
+    const newCats = [...cats];
+    newCats.splice(newCats.length, 0, "Broom");
+    return newCats;
+}
+
+function prependCat(name){
+    const newCats = [ ...cats];
+    newCats.splice(0, 0, "Arnold")
+    return newCats;
+}
+
+function removeLastCat(){
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat(){
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
